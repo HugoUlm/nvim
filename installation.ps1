b1ebc6fad099f38c086dd41e71aedbb9b3494647
Hugo Bäckman Ulmgren <hugo.backman.ulmgren@ica.se>
2024-12-05 16:26:03 +0100

    added ps1 script for install

---
+++ installation.ps1
@@ -0,0 +1,5 @@
+winget install --id=Neovim.Neovim  -e
+winget install --id=junegunn.fzf  -e
+winget install --id=BurntSushi.ripgrep.MSVC  -e
+winget install -e --id LLVM.LLVM
+winget install JanDeDobbeleer.OhMyPosh -s winget
