b92fd2b864aaef5b8f6d6ac581b83b1ef80d41cb
Hugo Bäckman Ulmgren <hugo.backman.ulmgren@ica.se>
2024-12-09 15:35:32 +0100

    added transparent rose pine

--- plugins/colors.lua
+++ plugins/colors.lua
@@ -10,14 +10,54 @@ return {
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
+		priority = 1000,
 		config = function()
 			require("rose-pine").setup({
-				disable_background = true,
+				variant = "moon", -- Options: 'auto', 'main', 'moon', 'dawn'
+				dark_variant = "moon", -- Options: 'main', 'moon', 'dawn'
+				dim_inactive_windows = false,
+				extend_background_behind_borders = false,
+				enable = {
+					terminal = true,
+					legacy_highlights = false,
+					migrations = false,
+				},
 				styles = {
-					italic = false,
+					bold = true,
+					italics = false,
+					transparency = true,
+				},
+				groups = {
+					border = "muted",
+					link = "iris",
+					panel = "surface",
+
+					error = "love",
+					hint = "iris",
+					info = "foam",
+					note = "pine",
+					todo = "rose",
+					warn = "gold",
+
+					git_add = "foam",
+					git_change = "rose",
+					git_delete = "love",
+					git_dirty = "rose",
+					git_ignore = "muted",
+					git_merge = "iris",
+					git_rename = "pine",
+					git_stage = "iris",
+					git_text = "rose",
+					git_untracked = "subtle",
+
+					h1 = "iris",
+					h2 = "foam",
+					h3 = "rose",
+					h4 = "gold",
+					h5 = "pine",
+					h6 = "foam",
 				},
 			})
-
 			ColorMyPencils()
 		end,
 	},
