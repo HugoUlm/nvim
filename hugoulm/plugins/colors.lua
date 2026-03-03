717e76b527d780a224acc79f16c7d550c8790ced
Hugo Ulmgren <94797431+HugoUlm@users.noreply.github.com>
2026-03-03 19:03:41 +0100

    Update colors.lua with new configuration

--- hugoulm/plugins/colors.lua
+++ hugoulm/plugins/colors.lua
@@ -1,41 +1,9 @@
-local utils = require("hugoulm.plugins.heirline.utils")
-
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	dir = vim.fn.stdpath("config"),
+	name = "prompt",
 	lazy = false,
 	priority = 10000,
-	opts = {
-		flavour = "mocha",
-		term_colors = true,
-		integrations = {
-			blink_cmp = true,
-			neotest = true,
-			noice = true,
-			diffview = true,
-			gitsigns = true,
-			snacks = { enabled = true },
-			lsp_trouble = true,
-			which_key = true,
-		},
-		lsp_styles = {
-			underlines = {
-				errors = { "undercurl" },
-				hints = { "undercurl" },
-				warnings = { "undercurl" },
-				information = { "undercurl" },
-				ok = { "underline" },
-			},
-		},
-		compile = {
-			enabled = true,
-		},
-		highlight_overrides = {
-			all = utils.hl_override
-		},
-	},
-	config = function(_, opts)
-		require("catppuccin").setup(opts)
-		vim.cmd.colorscheme("catppuccin")
+	config = function()
+		vim.cmd.colorscheme("prompt")
 	end,
 }
